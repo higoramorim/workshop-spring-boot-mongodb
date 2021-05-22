4c9e9eed0be6da18048991b795ab14c20b80790d
higoramorim <1810042@aluno.univesp.br>
2021-05-22 14:54:29 -0300

    put method

--- src/main/java/com/higoramorim/workshopmongo/resources/UserResource.java
+++ src/main/java/com/higoramorim/workshopmongo/resources/UserResource.java
@@ -34,7 +34,6 @@ public class UserResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
-
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 
@@ -49,7 +48,14 @@ public class UserResource {
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
-
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
 	}
 }
